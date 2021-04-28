fe1db2c906bc0acdd0312cd562b5d6c18b7a545a
israfil <israfil3019@gmail.com>
2021-04-29 00:06:07 +0300

    add keyUp and keyDown events

--- src/components/keyboardEvents/KeyboardEvents.js
+++ src/components/keyboardEvents/KeyboardEvents.js
@@ -1,11 +1,27 @@
-
 const KeyboardEvents = () => {
+  //keyDown
+  const handleKeyDown = (e) => {
+    console.log("keydown: ", e.target.value);
+    console.log("keycode: ", e.keyCode)
+  };
+
+  //keyUp
+  const handleKeyUp = (e) => {
+    console.log("keyup: ", e.type);
+  };
+
 
   return (
     <div style={{ marginTop: "30px" }}>
-      <h2>
-        KeyboardEvents
-      </h2>
+      <h2>KeyboardEvents</h2>
+      <input
+       
+        onKeyUp={handleKeyUp}
+        onKeyDown={handleKeyDown}
+        type="text"
+        defaultValue="enter something"
+      />
+     
     </div>
   );
 };
