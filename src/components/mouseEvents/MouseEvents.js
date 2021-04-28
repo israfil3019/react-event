cd838c4e109f1e68f4833b3cf754ad4a20122e4a
israfil <israfil3019@gmail.com>
2021-04-29 00:04:53 +0300

    add mouseMove event

--- src/components/mouseEvents/MouseEvents.js
+++ src/components/mouseEvents/MouseEvents.js
@@ -1,17 +1,30 @@
+import {useState} from "react";
 import "./styles.css";
 
 const MouseEvents = () => {
+  const [coordX, setCoordX] = useState("")
+  const [coordY, setCoordY] = useState("")
+  
   //click event
   const handleClick = (e) => {
     console.log(`event Obj: ${e}`)
     console.log(`event type: ${e.type}`)
   }
+  
   //doubleClick event
   const handleDoubleClick = (e) => console.log("event type: ", e.type)
+  
+  //mouseMove event
+  const handleMouseMove = (e) => {
+    setCoordX(e.nativeEvent.offsetX)
+    setCoordY(e.nativeEvent.offsetY)
+  }
 
   return (
     <>
       <h2>MouseEvents</h2>
+      <p><span>X</span>and Y</p>
+      <p><span>{coordX}</span>and {coordY}</p>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <ul>
           <li id="todo-1" onClick={handleClick}>
@@ -20,7 +33,7 @@ const MouseEvents = () => {
           <li id="todo-2" onDoubleClick={handleDoubleClick}>
             todo item2<span>x</span>
           </li>
-          <li>
+          <li id="todo-3" onMouseMove={handleMouseMove}>
             todo item3<span>x</span>
           </li>
           <li>
