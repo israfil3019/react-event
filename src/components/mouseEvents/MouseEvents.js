ff9c1580e63a6fa746a74617faaa2036fb992317
israfil <israfil3019@gmail.com>
2021-04-28 17:36:16 +0300

    add click and double click events

--- src/components/mouseEvents/MouseEvents.js
+++ src/components/mouseEvents/MouseEvents.js
@@ -1,10 +1,36 @@
 import "./styles.css";
 
 const MouseEvents = () => {
- 
+  //click event
+  const handleClick = (e) => {
+    console.log(`event Obj: ${e}`)
+    console.log(`event type: ${e.type}`)
+  }
+  //doubleClick event
+  const handleDoubleClick = (e) => console.log("event type: ", e.type)
+
   return (
     <>
-     <h2>MouseEvents</h2>
+      <h2>MouseEvents</h2>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <ul>
+          <li id="todo-1" onClick={handleClick}>
+            todo item 1 <span>x</span>
+          </li>
+          <li id="todo-2" onDoubleClick={handleDoubleClick}>
+            todo item2<span>x</span>
+          </li>
+          <li>
+            todo item3<span>x</span>
+          </li>
+          <li>
+            todo item4<span>x</span>
+          </li>
+          <li>
+            todo item5<span>x</span>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
