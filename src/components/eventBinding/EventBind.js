1ad41cf955b77e6272cbb46dceba6d5639c5e182
israfil <israfil3019@gmail.com>
2021-04-28 16:03:57 +0300

    add binding in render-- regular and arrow func

--- src/components/eventBinding/EventBind.js
+++ src/components/eventBinding/EventBind.js
@@ -1,11 +1,26 @@
 import { Component } from 'react'
 
 export default class EventBind extends Component {
-   
+   constructor(props){
+       super(props)
+       this.state = {message: "Hello"}
+   }
+//    handleClick(){
+//        this.setState({message: "Goodbye"})
+//        console.log("this: ", this);
+//    }
+    
+    handleClick(name ="john"){
+        this.setState({message: `Goodbye ${name}`})
+        console.log("this: ", this)
+    }
     render() {
         return (
             <div>
+               <p>{this.state.message}</p>
                <h2>EventBinding</h2>
+               <button onClick={this.handleClick.bind()}>Click1</button>
+               <button onClick={() => this.handleClick("jane")}>Click2</button>
             </div>
         )
     }
